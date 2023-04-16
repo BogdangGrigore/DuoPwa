1a62603895e0f8e6c152befbc23639d9d0f5361c
BogdangGrigore <76474544+BogdangGrigore@users.noreply.github.com>
2023-04-17 00:59:51 +0200

    first commit

---
+++ service-worker.js
@@ -0,0 +1,114 @@
+// service-worker.js
+
+const CACHE_NAME = 'my-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/camera.html',
+  '/camera.css' ,
+  '/script.js',
+  '/loginbackground.jpeg',
+  '/startpage.jpeg',
+  '/login.html',
+  '/login.css',
+  '/menu.html',
+  '/menu.css',
+  '/monalisa.html',
+  '/monalisa.css',
+  '/starry.html',
+  '/starry.css',
+  '/venus.html',
+  '/venus.css',
+  '/app.js',
+  '/image1.jpg',
+  '/menu.js',
+
+
+
+  
+  
+  
+  
+  // add a fallback page for offline content
+];
+
+window.addEventListener('beforeinstallprompt', (event) => {
+    // Prevent the mini-infobar from appearing on mobile.
+    event.preventDefault();
+    console.log('👍', 'beforeinstallprompt', event);
+    // Stash the event so it can be triggered later.
+    window.deferredPrompt = event;
+    // Remove the 'hidden' class from the install button container.
+    divInstall.classList.toggle('hidden', false);
+  });
+  butInstall.addEventListener('click', async () => {
+    console.log('👍', 'butInstall-clicked');
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+      // The deferred prompt isn't available.
+      return;
+    }
+    // Show the install prompt.
+    promptEvent.prompt();
+    // Log the result
+    const result = await promptEvent.userChoice;
+    console.log('👍', 'userChoice', result);
+    // Reset the deferred prompt variable, since
+    // prompt() can only be called once.
+    window.deferredPrompt = null;
+    // Hide the install button.
+    divInstall.classList.toggle('hidden', true);
+  });
+  window.addEventListener('appinstalled', (event) => {
+    console.log('👍', 'appinstalled', event);
+    // Clear the deferredPrompt so it can be garbage collected
+    window.deferredPrompt = null;
+  });
+
+self.addEventListener('install', (event) => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // If not found in cache, fetch from network
+        return fetch(event.request)
+          .then((response) => {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response
+            const responseToCache = response.clone();
+
+            // Add response to cache
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          })
+          .catch(() => {
+            // If both cache and network fail, return offline fallback page
+            return caches.match('/offline.html');
+          });
+      })
+  );
+});
